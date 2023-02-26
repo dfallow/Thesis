59ec6a7a091aa42f7030aa03c4287d96a0439c53
dfallow <dfallow@protonmail.com>
2023-02-26 16:57:50 +0200

    fix script issue

--- scripts/provisionDevice.sh
+++ scripts/provisionDevice.sh
@@ -13,7 +13,7 @@ tpm2_flushcontext -Q -l
 
 # Generate EK and AK
 tpm2_createek -c $EKHANDLE -G rsa -u /tmp/ek.pub
-tpm2_createak -c $EKHANDLE -c /tmp/ak.ctx -G rsa -g sha256 -s rsassa -u /tmp/ak.pub -f pem -n /tmp/ak.name
+tpm2_createak -C $EKHANDLE -c /tmp/ak.ctx -G rsa -g sha256 -s rsassa -u /tmp/ak.pub -f pem -n /tmp/ak.name
 tpm2_evictcontrol -c /tmp/ak.ctx $AKHANDLE
 
 
