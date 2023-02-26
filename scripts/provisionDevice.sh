254851b851a50ab139d6ae1e350c420fd41c5ab0
dfallow <dfallow@protonmail.com>
2023-02-26 16:34:21 +0200

    script modified

--- scripts/provisionDevice.sh
+++ scripts/provisionDevice.sh
@@ -4,12 +4,17 @@ EKHANDLE=0x810100ee
 AKHANDLE=0x810100aa
 
 
-#tpm2_evictcontrol -Q -c $EKHANDLE
-#tpm2_evictcontrol -Q -c $AKHANDLE
+tpm2_evictcontrol -Q -c $EKHANDLE
+tpm2_evictcontrol -Q -c $AKHANDLE
 
-#tpm2_flushcontext -Q -t
-#tpm2_flushcontext -Q -s
-#tpm2_flushcontext -Q -l
+tpm2_flushcontext -Q -t
+tpm2_flushcontext -Q -s
+tpm2_flushcontext -Q -l
+
+# Generate EK and AK
+tpm2_createek -c $EKHANDLE -G rsa -u /tmp/ek.pub
+tpm2_createak -c $EKHANDLE -c /tmp/ak.ctx -G rsa -g sha256 -s rsassa -u /tmp/ak.pub -f pem -n /tmp/ak.name
+tpm2_evictcontrol -c /tmp/ak.ctx $AKHANDLE
 
 
 file=$(mktemp -t deviceInfo.XXXXXX)
